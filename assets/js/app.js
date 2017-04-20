49cf2d02ac80b8a05fb8bb27dbfdd059d9047b80
RuthMery <ruth.cardenas.perez@gmail.com>
2017-04-20 11:38:49 -0500

    app-refactorización

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,21 @@
 //VARIABLES GLOBALES
   var container = document.getElementById('container');
-  printCubes(container);
-  printCubes(container);
+  printCubes(2, container);
 
 //FUNCIONES
 function createCube (parent, classList) {
   var cube = document.createElement('div');
-  cube.classList.add(classList[0]);
-  cube.classList.add(classList[1]);
+  for (var i = 0; i < classList.length; i++) {
+    cube.classList.add(classList[i]);
+  }
   parent.appendChild(cube);
   return cube;
 }
 
-function printCubes(container) {
-  var cube = createCube (container, ["cube-big", "cube-center"]);
-  createCube (cube, ["cube-medium", "cube-absolute"]);
-  createCube (cube, ["cube-small", "cube-absolute"]);
+function printCubes(numberCubes, container) {
+  for (var i = 0; i < numberCubes; i++) {
+    var cube = createCube (container, ["cube-big", "cube-center"]);
+    createCube (cube, ["cube-medium", "cube-absolute"]);
+    createCube (cube, ["cube-small", "cube-absolute"]);
+  }
 }
